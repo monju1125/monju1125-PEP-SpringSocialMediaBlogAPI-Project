5f47cd193721de96418167ceb0808e939a30dc3f
monju1125 <monju1125@github.com>
2026-07-04 23:00:49 +0000

    Register repositories as Spring managed components

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,16 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entity.Message;
+import java.util.*;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    List<Message> findByPostedByOrderByMessageIdAsc(Integer postedBy);
+
+    List<Message> findAllByOrderByMessageIdAsc();
+
 }
